e67d20533ff5a1fd6696f48e30b543480aaf6dd9
Sam <sam@sambhattacharyya.com>
2023-08-20 16:44:49 -0500

    Added basic hello world output

--- public/main.js
+++ public/main.js
@@ -1,13 +1,62 @@
+const canvas = document.querySelector("canvas");
 
 
 
-console.log("Hello world");
+async function main() {
 
-navigator.gpu.requestAdapter().then(function (adapter) {
+    if (!navigator.gpu) return console.log("WebGPU not supported");
 
-    console.log("Got WebGPU adapter")
+    const adapter = await navigator.gpu.requestAdapter();
 
-    console.log(adapter);
+    if(!adapter) return console.log("Unable to get adapter");
+    else console.log("Got WebGPU adapter", adapter);
 
-});
+    const device = await adapter.requestDevice();
+
+    if(!device) return console.log("Unable to get GPU Device");
+    else console.log("Got WebGPU Device", device);
+
+
+
+    const context = canvas.getContext("webgpu");
+
+    if(!context) return console.log("Unable to get WebGPU context");
+    else console.log("Got WebGPU Context", context);
+
+    const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+    console.log("Canvas format");
+    console.log(canvasFormat);
+
+    context.configure({
+        device: device,
+        format: canvasFormat,
+    });
+
+    console.log("Configured canvas");
+
+
+    const encoder = device.createCommandEncoder();
+
+    console.log("Create GPU command encoder");
+    console.log(encoder);
+
+    const pass = encoder.beginRenderPass({
+        colorAttachments: [{
+            view: context.getCurrentTexture().createView(),
+            loadOp: "clear",
+            storeOp: "store",
+            clearValue: { r: 0.2, g: 0, b: 1, a: 1 }
+        }]
+    });
+
+    pass.end();
+
+    const commandBuffer = encoder.finish();
+
+    device.queue.submit([commandBuffer]);
+
+}
+
+
+document.addEventListener("DOMContentLoaded", main);
 
